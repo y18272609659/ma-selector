fdca2c5026581993b4bf307727d3b77437b5808c
xiaoman <752453143@qq.com>
2019-01-02 16:36:15 +0800

    改进testing页面的算法，并为testing组件配置独立路由，在表格未完成情况下离开页面，路由会弹出选择框

--- src/pages/testing/testing.vue
+++ src/pages/testing/testing.vue
@@ -41,36 +41,72 @@ import HomeFooter from '../Hello/component/footer'
             }
         },
         methods: {
-
-            // handleCheckout函数做两件事：
-            // 1.给被点击的按钮一个class，使其有独特样式。
-            // 2.进行运算，并把数据按顺序排列在allNumber数组里。
-
             handleCheckout (item1,item2,item3) {
-                for (let i = 0 ; i < this.list.length ; i++){
-                    if (item1 === this.list[i]){
-                        for(let j = 0 ; j < this.factor.length ; j++) {
+                for (let i = 0 ; i < this.list.length ; i++ ) {
+                    if (item1 === this.list[i]) {
+                        for (let j = 0 ; j < this.factor.length ; j++) {
                             if (item2 === this.factor[j]) {
-
-                                // 为被点击的Number按钮添加class='checkout'，使其有独特的样式。
                                 let List = document.getElementsByClassName('list')[i];
-                                let Factor = List.getElementsByClassName('factor')[j];
-                                let num = Factor.getElementsByClassName('number')[item3-1];
-                                num.setAttribute('class','checkout');
-                                // num.className === 'number' ? num.setAttribute('class','number checkout') : num.setAttribute('class','number');
-                                // num.classList.add('checkout');
-
-                                // 收集被点击的Number按钮的数值，并和相关权重一起计算，最后放置在allNumber数组里，allNumber数组的数值顺序与list数组的选项顺序一一对应。
-                                if ( this.allNumber[i] ) {
-                                    this.allNumber[i] += (this.factor.length - j) * item3
-                                } else {
-                                    this.allNumber[i] = (this.factor.length - j) * item3
-                                }
+                                    let Factor = List.getElementsByClassName('factor')[j];
+                                    let old = Factor.getElementsByClassName('checkout')[0];
+                                    let num = Factor.getElementsByTagName('button')[item3-1];
+                                        if (!old) {
+                                            num.setAttribute('class','checkout')
+                                        } else {
+                                            old.setAttribute('class','number');
+                                            num.setAttribute('class','checkout');
+                                        }
                             }
                         }
                     }
                 }
             },
+
+            handleReport () {
+                for (let i = 0 ; i < this.list.length ; i++){
+                        this.allNumber[i] = 0
+                        let Factor = document.getElementsByClassName('list')[i].getElementsByClassName('factor');
+                            for (let j = 0 ; j < Factor.length ; j++) {
+                                let Num = Factor[j].getElementsByTagName('button')
+                                for (let k = 0 ; k < Num.length ; k++) {
+                                    if ( Num[k] === Factor[j].getElementsByClassName('checkout')[0] ) {
+                                        this.allNumber[i] += ((this.factor.length - j) * ( k + 1 ));
+                                    }
+                                }
+                            }
+                        }
+                    localStorage.setItem('allNumber',JSON.stringify(this.allNumber))
+                    }
+
+            // handleCheckout函数做两件事：
+            // 1.给被点击的按钮一个class，使其有独特样式。
+            // 2.进行运算，并把数据按顺序排列在allNumber数组里。
+
+            // handleCheckout (item1,item2,item3) {
+            //     for (let i = 0 ; i < this.list.length ; i++){
+            //         if (item1 === this.list[i]){
+            //             for(let j = 0 ; j < this.factor.length ; j++) {
+            //                 if (item2 === this.factor[j]) {
+
+            //                     // 为被点击的Number按钮添加class='checkout'，使其有独特的样式。
+            //                     let List = document.getElementsByClassName('list')[i];
+            //                     let Factor = List.getElementsByClassName('factor')[j];
+            //                     let num = Factor.getElementsByClassName('number')[item3-1];
+            //                     num.setAttribute('class','checkout');
+            //                     // num.className === 'number' ? num.setAttribute('class','number checkout') : num.setAttribute('class','number');
+            //                     // num.classList.add('checkout');
+
+            //                     // 收集被点击的Number按钮的数值，并和相关权重一起计算，最后放置在allNumber数组里，allNumber数组的数值顺序与list数组的选项顺序一一对应。
+            //                     if ( this.allNumber[i] ) {
+            //                         this.allNumber[i] += (this.factor.length - j) * item3
+            //                     } else {
+            //                         this.allNumber[i] = (this.factor.length - j) * item3
+            //                     }
+            //                 }
+            //             }
+            //         }
+            //     }
+            // },
             // 该函数检测表格是否填写完，若没填写完，则给用户一个alert弹窗提示。（其功能待完善)
             // handleReport () {
             //     if ( this.allNumber.length === this.list.length) {
@@ -81,8 +117,8 @@ import HomeFooter from '../Hello/component/footer'
             // },
         },
         beforeRouteLeave (to, from , next) {
-            if( this.allNumber.length !== this.list.length ) {
-                const answer = window.confirm('喂！Σ(っ°Д°;)っ！表格还剩' + (this.list.length-this.allNumber.length) + '项没有填完!')
+            if( document.getElementsByClassName('checkout').length < this.list.length * this.factor.length ) {
+                const answer = window.confirm('喂！Σ(っ°Д°;)っ！表格还剩' + (this.list.length * this.factor.length - document.getElementsByClassName('checkout').length) + '项没有填完! 你确定要离开吗？')
                 if (answer) {
                     next()
                 } else {
